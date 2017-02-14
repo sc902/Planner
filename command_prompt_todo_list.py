059c45952bc409f630ab2bfc8037a72a832b35df
sc902 <23scherian@castilleja.org>
2017-02-13 17:56:54 -0800

    Functions
    Added some functions.

--- command_prompt_todo_list.py
+++ command_prompt_todo_list.py
@@ -2,53 +2,65 @@
 to_do_list = []
 completed = []
 
-'''
-# Version 1:
-number_tasks = int(raw_input("How many tasks do you have?"))
+def print_tasks():
+    print to_do_list
 
-print number_tasks
-count=0
-#Begin loop
-while(count<number_tasks):
-    #Ask user for input
+def add_task():
     task = raw_input("What is your task?")
-    #Add to list
     to_do_list.append(task)
-    count= count +1
-#Print tasks
-print to_do_list
+
+def print_enumerated_tasks():
+    for i, task in enumerate(to_do_list):
+        print i, task
+
+def complete_task():
+    print_enumerated_tasks()
+    task_index = int(raw_input("To complete a task, type the number of that task. "))
+    task = to_do_list.pop(task_index)
+    completed.append(task)
+
+def view_completed():
+    print completed
 
 
-while(0<number_tasks):
-    number_tasks= number_tasks -1
-    print number_tasks'''
+def remove_task():
+    print_enumerated_tasks()
+    task_index = int(raw_input("To remove a task, type the number of that task. "))
+    to_do_list.pop(task_index)
+
 
 while (True):
     # casting the string input to an integer
     option = int(raw_input("Choose an option:\n"
                            "1. Add a task\n"
                            "2. Print tasks\n"
-                           "3. Exit app\n"
-                           "4. Complete a task\n"))
+                           "3.Remove a task\n"
+                           "4. Complete a task\n"
+                           "5. View your completed tasks\n"
+                           "6. Exit app\n"))
     # 1. Add a task
+
     if option == 1:
-        task = raw_input("What is your task?")
-        to_do_list.append(task)
+        add_task()
     # 2. Print Tasks
     elif option == 2:
-        print to_do_list
-    # 3. Exit
+        print_tasks()
+    #Remove a task
     elif option == 3:
-        break
+        remove_task()
     #4. Complete a task
     elif option == 4:
-        for i, task in enumerate(to_do_list):
-            print i, task
-        task_index = int(raw_input("To complete a task, type the number of that task. "))
-        task = to_do_list.pop(task_index)
-        completed.append(task)
+        complete_task()
+    #5: View completed tasks
+    elif option == 5:
+        view_completed()
+    # 6. Exit
+    elif option == 6:
+        break
     else:
         print "I don't know."
 
-# HW: Work on MAGIC Presentation- hopefully finish
+#HW: Go through other requirements
+
+
 
