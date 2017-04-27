c7feed76e86a2e9037ed8a086ed936c367f708f8
sc902 <23scherian@castilleja.org>
2017-04-26 18:44:22 -0700

    Checkbutton Problem
    In this, I added a To-Do List title, iterated over the completed list
    and had a problem with the option portion of making a check button.

--- todo_list_gui.py
+++ todo_list_gui.py
@@ -5,17 +5,30 @@ from task_classes import Task, TaskList
 top = tk.Tk()
 my_task_list = TaskList()
 #DO NOT TOUCH ANYTHING ABOVE THIS
+var = tk.StringVar()
+label = tk.Label(top, textvariable=var, relief=tk.RAISED)
 
+var.set("To-Do List:")
+label.pack()
 #1. Iterate over not_done
 for task in my_task_list.not_done:
     var = tk.StringVar()
-    label = tk.Label(top, textvariable=var, relief=tk.RAISED)
+    label = tk.Label(top, textvariable=var, relief=tk.RAISED, bg = "yellow")
     #Printing description
     var.set(task.description)
     label.pack()
 
-#3. Make each element into a label
+for task in my_task_list.not_done:
+    w = tk.Checkbutton(tk.Tk(), tk.activeforeground)
+    w.place(x = 10, y = 10)
 
+#1. Iterate over done
+for task in my_task_list.done:
+    var = tk.StringVar()
+    label = tk.label = tk.Label( top, textvariable=var, relief=tk.RAISED, bg = "pink" )
+    #Printing description
+    var.set(task.description)
+    label.pack()
 
 #DO NOT TOUCH ANYTHING BELOW THIS
 top.mainloop()
