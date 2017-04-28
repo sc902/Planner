4eb0281478710963d9025ef29d3aab57941f09e9
sc902 <23scherian@castilleja.org>
2017-04-27 19:01:39 -0700

    Checkbutton Fixed

--- todo_list_gui.py
+++ todo_list_gui.py
@@ -8,14 +8,8 @@ my_task_list = TaskList()
 top.title("To-Do List")
 #1. Iterate over not_done
 for task in my_task_list.not_done:
-    var = tk.StringVar()
-    label = tk.Label(top, textvariable=var, relief=tk.RAISED, bg = "yellow")
-    #Printing description
-    var.set(task.description)
-    label.pack()
-    b = tk.Checkbutton(top)
-    b.pack(side = tk.LEFT, anchor = tk.N)
-
+    b = tk.Checkbutton(top, text = task.description, bg = "yellow")
+    b.pack()
 
 #1. Iterate over done
 for task in my_task_list.done:
