7a4454e852b107fedd14e3710a29b79fcffa9ade
sc902 <23scherian@castilleja.org>
2017-04-29 14:50:00 -0700

    Linking Buttons
    In this version, I made buttons to save and remove tasks, and linked
    them.

--- todo_list_gui.py
+++ todo_list_gui.py
@@ -12,15 +12,18 @@ e.pack(side = tk.TOP)
 for task in my_task_list.not_done:
     b = tk.Checkbutton(top, text = task.description, bg = "yellow")
     b.pack()
+    remove = tk. Button(top, text = "Remove", command = my_task_list.remove_task)
+    remove.pack()
 
 #1. Iterate over done
 for task in my_task_list.done:
     var = tk.StringVar()
-    label = tk.label = tk.Label( top, textvariable=var, relief=tk.RAISED, bg = "pink" )
+    label = tk.label = tk.Label( top, textvariable=var, relief=tk.RAISED, bg = "pink")
     #Printing description
     var.set(task.description)
     label.pack()
 
-
+save = tk.Button(top, text ="Save", command = my_task_list.save_tasks)
+save.pack()
 #DO NOT TOUCH ANYTHING BELOW THIS
 top.mainloop()
